9e732cc7ac05e2377fe803d89e2953be138cb01e
yatelu1 <yatelu1@hotmail.com>
2013-06-23 17:12:01 +0800

    update  last page

--- assets/js/upload.js
+++ assets/js/upload.js
@@ -9,14 +9,14 @@ if (typeof Aotulife !== 'object') var Aotulife = {};
 Aotulife.upload = new (function () {
 
     this.onReady = function () {
-        $("input.title").watermark("视频标题：");
-        $("textarea.info").watermark("视频介绍：");
+        $("input.title").watermark("关于视频说点什么吧：");
+        $("textarea.info").watermark("分享：");
         $("input.tag").watermark("视频标签：");
         $("input.link").watermark("输入宝贝淘宝链接：");
         $("input.price").watermark("填写宝贝价格：");
 
 
-        var textButton = "<input type='text' name='textfield' id='textfield' class='input file' /> &nbsp;<input type='submit' name='button' id='button' value='浏览本地文件' class='btns' />"
+        var textButton = "<input type='text' name='textfield' id='textfield' class='input file' /> &nbsp;<input type='submit' name='button' id='button' value='上传' class='btns' />"
         $(textButton).insertBefore("#fileField");
         $("#fileField").change(function () {
             $("#textfield").val($("#fileField").val());
